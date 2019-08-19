cebd0e6252efb466507f296cfc9f6c3022288fc8
Austin Walela <austinmasibo@gmail.com>
2019-08-19 15:12:57 +0300

    completed MVP tasks for function-conversion.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,32 @@
 // };
 // myFunction();
 
+let myFunction = () => console.log("Function was invoked!");
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = param => param;
+console.log(anotherFunction("Hello!"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1 + param2;
+console.log(add(5, 10));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let substract = (param1, param2) => param1 - param2;
+console.log(substract(20, 9));
 
 // Stretch
 
