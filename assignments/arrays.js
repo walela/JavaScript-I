b9f511743efc296277ffe2c7c0e39e2b06cab8c2
Austin Walela <austinmasibo@gmail.com>
2019-10-15 08:35:05 +0300

    complete mvp and fix merge conflict

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -85,26 +85,16 @@ let inventory = [
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-<<<<<<< HEAD
+
 let car33 = inventory.find(car => car.id === 33);
 console.log(
   `Car 33 is a ${car33.car_year} ${car33.car_make} ${car33.car_model}.`
-=======
-console.log(
-  `Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${
-    inventory[32].car_model
-  }.`
->>>>>>> ee61ad3af4a3073e8934bfe3081c4723235148ec
 );
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = inventory[inventory.length - 1];
-<<<<<<< HEAD
 console.log(`The last car is a ${lastCar.car_make} ${lastCar.car_model}.`);
-=======
-console.log(`The last car is a ${lastCar.car_make} ${lastCar.car_model}`);
->>>>>>> ee61ad3af4a3073e8934bfe3081c4723235148ec
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
